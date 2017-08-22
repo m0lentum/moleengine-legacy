4c1c0b6e5f8c4dbb4bfc90948c3629fdd4b42e7d
Mikael Myyrä <mikael.myyrae@gmail.com>
2017-08-22 21:29:30 +0300

    getTexture returns null on nonexistent texture
    Previously it would throw an error.

--- src/ME/AssetManager.cpp
+++ src/ME/AssetManager.cpp
@@ -1,27 +1,32 @@
 #include <AssetManager.hpp>
-//#include <iostream>
+#include <stdexcept>
+#include <iostream>
 
 namespace me
 {
 	void AssetManager::loadTexture(const std::string &path, const std::string &name)
 	{
-		//Only do anything if the name is unique
-		if (m_textures.count(name) <= 0)
-		{
-			std::shared_ptr<sf::Texture> texture = std::make_shared<sf::Texture>();
-			texture->loadFromFile(path);
-			m_textures[name] = texture;
-		}
+		std::shared_ptr<sf::Texture> texture = std::make_shared<sf::Texture>();
+		texture->loadFromFile(path);
+		m_textures[name] = texture;
 	}
 
 	std::shared_ptr<sf::Texture> AssetManager::getTexture(const std::string &name) const
 	{
-		return m_textures.at(name); //this will throw an exception if the key doesn't exist
+		try
+		{
+			return m_textures.at(name);
+		}
+		catch (const std::out_of_range&)
+		{
+			std::cerr << "Error: attempted to get a texture that doesn't exist: " << name << std::endl;
+			return NULL;
+		}
 	}
 
 	void AssetManager::cleanup()
 	{
-		//loop through all textures and see if they're referenced anywhere
+		//loop through all textures and see if they're referenced anywhere, delete if not
 		for (auto iter = m_textures.begin(); iter != m_textures.end(); iter++)
 		{
 			if (iter->second.use_count() <= 1)

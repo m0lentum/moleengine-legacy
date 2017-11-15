0b65cfbeda177cea81c609c7428627a0a770228b
Mikael Myyrä <mikael.myyrae@gmail.com>
2017-11-15 19:28:27 +0200

    Added function to align polygon with center of mass

--- include/ME/Physics/ColliderPolygon.hpp
+++ include/ME/Physics/ColliderPolygon.hpp
@@ -12,11 +12,18 @@ namespace me
 	{
 	private:
 		
-		std::vector<sf::Vector2f> m_points;
+		std::vector<sf::Vector2f> m_edges;
 
 	public:
 
+		const std::vector<sf::Vector2f>& getPoints() const;
+
 		const sf::Vector2f& getPosition() const;
+
+		/// Calculate the area of the polygon.
+		float getArea();
+		/// Move the origin of the polygon to its centroid (center of mass).
+		void center();
 
 		virtual void findTypeAndCheckCollision(const ICollider &other, CollisionInfo &info) const;
 
@@ -28,10 +35,21 @@ namespace me
 
 		virtual ICollider* clone() const;
 
-		/// Constructor that takes vectors
-		ColliderPolygon(std::initializer_list<sf::Vector2f> points);
+	private:
+
+		/// Input: points as the coordinates of each vertex
+		/// Output: the vector from the origin to the first point followed by the edges of the polygon
+		static std::vector<sf::Vector2f> pointsToEdges(const std::vector<sf::Vector2f> &points);
+		/// PointsToEdges in reverse
+		static std::vector<sf::Vector2f> edgesToPoints(const std::vector<sf::Vector2f> &edges);
+
+	public:
+
+		/// Constructor that takes vectors.
+		/// If autoCenter, the collider will be centered around its center of mass.
+		ColliderPolygon(std::initializer_list<sf::Vector2f> points, bool autoCenter = true);
 		/// Constructor that takes a list of coordinates
-		ColliderPolygon(std::initializer_list<float> coords);
+		ColliderPolygon(std::initializer_list<float> coords, bool autoCenter = true);
 		/// Copy constructor
 		ColliderPolygon(const ColliderPolygon &copy);
 		virtual ~ColliderPolygon() {}
--- src/ME/Physics/ColliderPolygon.cpp
+++ src/ME/Physics/ColliderPolygon.cpp
@@ -7,10 +7,55 @@
 
 namespace me
 {
+	const std::vector<sf::Vector2f>& ColliderPolygon::getPoints() const
+	{
+		return m_edges;
+	}
+
 	const sf::Vector2f& ColliderPolygon::getPosition() const
 	{
 		return m_parent->getPosition();
 	}
+
+	float ColliderPolygon::getArea()
+	{
+		float getArea = 0;
+		sf::Vector2f curr, next; // the sum of edges so far == vector from the first point to the current one
+		for (std::vector<sf::Vector2f>::size_type i = 1; i < m_edges.size() - 1; i++)
+		{
+			curr += m_edges[i];
+			next = curr + m_edges[i + 1];
+
+			getArea += std::abs(curr.x * next.y - curr.y * next.x);
+		}
+
+		return getArea / 2;
+	}
+
+	void ColliderPolygon::center()
+	{
+		float area = getArea();
+		
+		// Calculate the centroid relative to the first point of the polygon
+		sf::Vector2f weightedSumOfC, curr, next;
+		for (std::vector<sf::Vector2f>::size_type i = 1; i < m_edges.size() - 1; i++)
+		{
+			curr += m_edges[i];
+			next = curr + m_edges[i + 1];
+
+			sf::Vector2f ctr = curr + next; // this is actually 3 times the centroid, but we will do the division later
+			float weight = std::abs(curr.x * next.y - curr.y * next.x);
+
+			weightedSumOfC += ctr * weight;
+		}
+
+		sf::Vector2f centroid = weightedSumOfC / (6 * area);
+
+		m_edges[0] = -centroid; // the first point in the edges represents the centroid
+	}
+
+
+
 
 	void ColliderPolygon::checkCollision(const ColliderCircle &other, CollisionInfo &info) const
 	{
@@ -34,7 +79,7 @@ namespace me
 
 	Graphic* ColliderPolygon::generateGraphic(const sf::Color &color) const
 	{
-		return Graphic::makePolygon(m_points, color);
+		return Graphic::makePolygon(edgesToPoints(m_edges), color);
 	}
 
 
@@ -44,12 +89,43 @@ namespace me
 	}
 
 
-	ColliderPolygon::ColliderPolygon(std::initializer_list<sf::Vector2f> points) :
-		m_points(points)
+	std::vector<sf::Vector2f> ColliderPolygon::pointsToEdges(const std::vector<sf::Vector2f> &points)
 	{
+		std::vector<sf::Vector2f> edges;
+
+		edges.push_back(points[0]); // the first vector in this format is the vector from the polygon's origin to its first point
+		for (std::vector<sf::Vector2f>::size_type i = 1; i < points.size(); i++)
+		{
+			edges.push_back(points[i] - points[i - 1]); // the edge between the two points
+		}
+
+		return edges;
 	}
 
-	ColliderPolygon::ColliderPolygon(std::initializer_list<float> coords)
+	std::vector<sf::Vector2f> ColliderPolygon::edgesToPoints(const std::vector<sf::Vector2f> &edges)
+	{
+		std::vector<sf::Vector2f> points;
+		
+		points.push_back(edges[0]);
+		sf::Vector2f sum = edges[0];
+		for (std::vector<sf::Vector2f>::size_type i = 1; i < edges.size(); i++)
+		{
+			sum += edges[i]; // the sum of edges up to this point is the position of the point
+			points.push_back(sum);
+		}
+
+		return points;
+	}
+
+
+
+	ColliderPolygon::ColliderPolygon(std::initializer_list<sf::Vector2f> points, bool autoCenter) :
+		m_edges(pointsToEdges(points))
+	{
+		if (autoCenter) center();
+	}
+
+	ColliderPolygon::ColliderPolygon(std::initializer_list<float> coords, bool autoCenter)
 	{
 		auto end = coords.end();
 		if (coords.size() % 2 != 0)
@@ -57,9 +133,15 @@ namespace me
 			end--; // Ignore the last argument if the number of coordinates given is odd
 			std::cerr << "Warning: odd number of coordinates given to ColliderPolygon." << std::endl;
 		}
+
+		std::vector<sf::Vector2f> points;
 		for (auto i = coords.begin(); i != end; i += 2) {
-			m_points.push_back(sf::Vector2f(*i, *(i + 1)));
+			points.push_back(sf::Vector2f(*i, *(i + 1)));
 		}
+
+		m_edges = pointsToEdges(points);
+
+		if (autoCenter) center();
 	}
 
 	ColliderPolygon::ColliderPolygon(const ColliderPolygon &copy)

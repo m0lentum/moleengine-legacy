3428d7b4cf7963d872cf947c094125046b611912
Mikael Myyrä <mikael.myyrae@gmail.com>
2017-12-09 17:51:58 +0200

    Added Renderer for drawing Graphics

--- include/ME/Graphics/Renderer.hpp
+++ include/ME/Graphics/Renderer.hpp
@@ -1,24 +1,28 @@
-#ifndef GRAPHIC_CONTROLLER_HPP
-#define GRAPHIC_CONTROLLER_HPP
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
 
 #include "../ISystem.hpp"
-#include "Graphic.hpp"
-#include "AnimatedSprite.hpp"
-#include <vector>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/Transform.hpp>
+#include <SFML/System/Time.hpp>
 
 namespace me
 {
+	class Graphic;
+
 	class Renderer : public ISystem
 	{
 	public:
 
-		virtual void continuousUpdate(sf::Time timeElapsed);
-		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+		virtual void continuousUpdate(sf::Time timeElapsed) override;
+		virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
+		void drawGraphic(Graphic *graphic, const sf::Transform &transform, sf::RenderTarget &target, sf::RenderStates states) const;
 
 		virtual ~Renderer() {}
 	};
 }
 
 
-#endif // GRAPHIC_CONTROLLER_HPP
+#endif // RENDERER_HPP
--- src/ME/Graphics/Renderer.cpp
+++ src/ME/Graphics/Renderer.cpp
@@ -1,14 +1,36 @@
 #include <Graphics/Renderer.hpp>
-#include <iostream>
+#include <Graphics/Graphic.hpp>
+#include <Graphics/AnimatedSprite.hpp>
+#include <Space.hpp>
+#include <ComponentStorageUnit.hpp>
+#include <GameObject.hpp>
+
 namespace me
 {
 	void Renderer::continuousUpdate(sf::Time timeElapsed)
 	{
-
+		m_space->each<AnimatedSprite>([&](ComponentStorageUnit<AnimatedSprite> &unit)
+		{
+			unit.getComponent()->continuousUpdate(timeElapsed);
+		});
 	}
 
 	void Renderer::draw(sf::RenderTarget &target, sf::RenderStates states) const
 	{
-		std::cout << "Hell yea" << std::endl;
+		m_space->each<Graphic>([&](ComponentStorageUnit<Graphic> &unit)
+		{
+			drawGraphic(unit.getComponent(), unit.getParent()->getTransform(), target, states);
+		});
+
+		m_space->each<AnimatedSprite>([&](ComponentStorageUnit<AnimatedSprite> &unit)
+		{
+			drawGraphic(unit.getComponent(), unit.getParent()->getTransform(), target, states);
+		});
+	}
+
+	void Renderer::drawGraphic(Graphic *graphic, const sf::Transform &transform, sf::RenderTarget &target, sf::RenderStates states) const
+	{
+		states.transform *= transform;
+		graphic->draw(target, states);
 	}
 }
